a0b05c1e6e88fc10b875c1ae5558edc3a6644668
Alisa Hasanli <alisahesenli19@gmail.com>
2022-04-04 22:52:52 +0200

    Preparation de Main

--- TP4/main.cpp
+++ TP4/main.cpp
@@ -18,26 +18,38 @@ int main(int argc, char** argv)
 
   uint64_t n,m;
 
-  n = 3;
-  m = 3;
+  n = 4;
+  m = 4;
 
-    /*** Allocations des matrices pour tester la triangularizations ***/
+    /*** Allocations des matrices ***/
     double* A = allocateMatrix(n,m);
     double* B = allocateVector(n);
-    A[0] = 1 ;  A[1] = -3 ; A[2] = 2 ;
-    A[3] = 2 ;  A[4] =  1 ; A[5] = 1 ;
-    A[6] = 3 ;  A[7] = -1 ; A[8] = 5 ;
+    double* S = allocateMatrix(n,m);
+    double* x = allocateVector(n);
+    A[0] = 10 ;  A[1]  = 7 ; A[2]  = 8  ; A[3]  = 7  ;
+    A[4] = 7  ;  A[5]  = 5 ; A[6]  = 6  ; A[7]  = 5  ;
+    A[8] = 8  ;  A[9]  = 6 ; A[10] = 10 ; A[11] = 9  ;
+    A[12] = 7 ;  A[13] = 5 ; A[14] = 9  ; A[15] = 10 ;
     
 
-    B[0] =  1;
-    B[1] = -2;
-    B[2] =  3;
+    B[0] = 32;
+    B[1] = 23;
+    B[2] = 33;
+    B[3] = 31;
+
+    /*** Test des fonctions ***/
+
+    matrixMultiplyNaive(S,A,B,n,m,n);
+
+    SolveTriangularSystemUP(x,A,B,n);
 
     Triangularize(A,B,n);
 
-    writeMatrix(stdout,A,n,m);
+    SolveSystemGauss(x,A,B,n);
 
     freeMatrix(A);
-    freeVector(B);   
+    freeMatrix(S);
+    freeVector(B);
+    freeVector(x);  
     return 0;
 }
